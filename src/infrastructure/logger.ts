02d2837d95004f7951e5feb56a1f29b34cd1ea6d
HuyvictoryTego <vm.huy@tego.global>
2023-07-23 19:32:33 +0700

    first commit: BE source init

---
+++ src/infrastructure/logger.ts
@@ -0,0 +1,12 @@
+import { configure, format, transports } from 'winston'
+
+configure({
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.json()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: process.env.API_LOG_FILENAME })
+  ]
+})
