54c81089e16fa142df4bfa785b4741d7cd36dedb
Huyvictory <vominhhuy008@gmail.com>
2023-09-17 16:58:51 +0700

    refactor: remove unused types channel_description, contactlink

--- src/contracts/channelDescription.ts
+++
@@ -1,9 +0,0 @@
-import { Model, ObjectId } from 'mongoose'
-
-export interface IChannelDescription {
-  descption_overview: string
-  descripton_contact_info: string
-  description_links: ObjectId[]
-}
-
-export type ChannelDescriptionModel = Model<IChannelDescription>
--- src/contracts/contactLink.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose'
-
-export interface IContactLink {
-  link_name: string
-  link_url: string
-}
-
-export type ContactLinkModel = Model<IContactLink>
